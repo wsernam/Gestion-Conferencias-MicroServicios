cb17441fad5301982a1cc2d4b8a5a86a9f991bba
wsernam <wsernam@unicauca.edu.co>
2024-10-17 22:16:26 -0500

    AgregeController para probar

--- notify-service/src/main/java/com/mycompany/notify/Main.java
+++ notify-service/src/main/java/com/mycompany/notify/Main.java
@@ -1,12 +1,13 @@
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
-
+package com.mycompany.notify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  *
  * @author wsern
