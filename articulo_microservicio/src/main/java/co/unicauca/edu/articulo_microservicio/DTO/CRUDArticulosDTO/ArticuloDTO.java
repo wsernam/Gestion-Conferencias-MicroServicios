060ae83e4ee79004f5d13371143f80614ca36bd2
sonhuila <mariapaulabm09@unicauca.edu.co>
2024-10-27 10:02:10 -0500

    agregue los test de articulo controller y articulo service impl

--- articulo_microservicio/src/main/java/co/unicauca/edu/articulo_microservicio/DTO/CRUDArticulosDTO/ArticuloDTO.java
+++ articulo_microservicio/src/main/java/co/unicauca/edu/articulo_microservicio/DTO/CRUDArticulosDTO/ArticuloDTO.java
@@ -26,4 +26,8 @@ public class ArticuloDTO {
     public ArticuloDTO()
     {
     }
+
+    public void setId(Integer idArticulo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
